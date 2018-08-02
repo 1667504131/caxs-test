9f7b0bc7964ce39b7f419a37ac693de48cb13e9f
dongjiawei <dongjiawei_qd@caxins.com>
2018-08-02 21:34:11 +0800

    提交

---
+++ caxs-api/src/main/java/com/caxs/base/service/IHmUserService.java
@@ -0,0 +1,11 @@
+package com.caxs.base.service;
+
+import com.caxs.base.domain.HmUser;
+
+public interface IHmUserService {
+    HmUser selectByLoginId(String loginId);
+
+    int insertHmUser(HmUser hmUser);
+
+    int updateById(HmUser hmUser);
+}
